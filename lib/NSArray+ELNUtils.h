e23f94713817bf3f063fbbc54ac0bf04a86d2678
sergey.rakov <sergey.rakov@e-legion.com>
2016-02-02 15:48:22 +0300

    add some array utils

--- lib/NSArray+ELNUtils.h
+++ lib/NSArray+ELNUtils.h
@@ -14,6 +14,10 @@
 - (ObjectType)eln_objectPassingTest:(BOOL (^)(ObjectType obj, NSUInteger idx, BOOL *stop))predicate;
 /// Returns object in the array passing the test considering specified options.
 - (ObjectType)eln_objectWithOptions:(NSEnumerationOptions)options passingTest:(BOOL (^)(ObjectType obj, NSUInteger idx, BOOL *stop))predicate;
+/// Returns objects in the array passing the test.
+- (NSArray<ObjectType> *)eln_objectsPassingTest:(BOOL (^)(ObjectType obj, NSUInteger idx, BOOL *stop))predicate;
+/// Returns objects in the array passing the test considering specified options.
+- (NSArray<ObjectType> *)eln_objectsWithOptions:(NSEnumerationOptions)options passingTest:(BOOL (^)(ObjectType obj, NSUInteger idx, BOOL *stop))predicate;
 
 /// Map objects to returned values, filter out those ones where nil is returned.
 - (NSArray *)eln_map:(id (^)(ObjectType obj, NSUInteger idx, BOOL *stop))predicate;
--- lib/NSArray+ELNUtils.m
+++ lib/NSArray+ELNUtils.m
@@ -11,7 +11,12 @@
 @implementation NSArray (ELNUtils)
 
 - (id)eln_objectPassingTest:(BOOL (^)(id, NSUInteger, BOOL *))predicate {
-    return [self eln_objectWithOptions:0 passingTest:predicate];
+    NSUInteger index = [self indexOfObjectPassingTest:predicate];
+    if (index == NSNotFound) {
+        return nil;
+    } else {
+        return self[index];
+    }
 }
 
 - (id)eln_objectWithOptions:(NSEnumerationOptions)options passingTest:(BOOL (^)(id obj, NSUInteger idx, BOOL *stop))predicate {
@@ -21,6 +26,14 @@
     } else {
         return self[index];
     }
+}
+
+- (NSArray *)eln_objectsPassingTest:(BOOL (^)(id obj, NSUInteger idx, BOOL *stop))predicate {
+    return [self objectsAtIndexes:[self indexesOfObjectsPassingTest:predicate]];
+}
+
+- (NSArray *)eln_objectsWithOptions:(NSEnumerationOptions)options passingTest:(BOOL (^)(id obj, NSUInteger idx, BOOL *stop))predicate {
+    return [self objectsAtIndexes:[self indexesOfObjectsWithOptions:options passingTest:predicate]];
 }
 
 - (NSArray *)eln_map:(id (^)(id obj, NSUInteger index, BOOL *stop))predicate {
@@ -41,6 +54,10 @@
         }
     }
     return [result copy];
+}
+
+- (BOOL)eln_any:(BOOL (^)(id, NSUInteger, BOOL *))predicate {
+    return [self eln_objectPassingTest:predicate] != nil;
 }
 
 @end
