25fd4fd17d31a13e68ff1727111c76882d8cc6e2
sergey.rakov <sergey.rakov@e-legion.com>
2016-02-03 12:27:33 +0300

    add common utils header

---
+++ lib/ELNCommonUtils.h
@@ -0,0 +1,12 @@
+//
+//  ELNCommonUtils.h
+//  utilTest
+//
+//  Created by Sergey Rakov on 03/02/16.
+//  Copyright © 2016 e-Legion. All rights reserved.
+//
+
+@import Foundation;
+
+/// NSString by selector.
+#define ELNSelectorString(x) NSStringFromSelector(@selector(x))
