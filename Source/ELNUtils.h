3722dedecd8b02bbd4fcc9e35a5977fd75dfeb3b
chebur <chebur.mail@gmail.com>
2016-03-01 19:01:02 +0300

    Add xcode project and reorganize files

---
+++ Source/ELNUtils.h
@@ -0,0 +1,19 @@
+//
+//  ELNUtils.h
+//  ELNUtils
+//
+//  Created by Dmitry Nesterenko on 01.03.16.
+//  Copyright © 2016 e-legion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ELNUtils.
+FOUNDATION_EXPORT double ELNUtilsVersionNumber;
+
+//! Project version string for ELNUtils.
+FOUNDATION_EXPORT const unsigned char ELNUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ELNUtils/PublicHeader.h>
+
+
