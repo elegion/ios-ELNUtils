16d90ff8004c6f9ba4ceeeaea68a6a74495a85e1
sergey.rakov <sergey.rakov@e-legion.com>
2016-04-22 20:45:58 +0300

    fill framework umbrella header

--- Source/ELNUtils.h
+++ Source/ELNUtils.h
@@ -14,6 +14,29 @@ FOUNDATION_EXPORT double ELNUtilsVersionNumber;
 //! Project version string for ELNUtils.
 FOUNDATION_EXPORT const unsigned char ELNUtilsVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <ELNUtils/PublicHeader.h>
+#import "ELNRuntime.h"
+#import "NSArray+ELNUtils.h"
+#import "NSBundle+ELNUtils.h"
+#import "NSData+ELNUtils.h"
+#import "NSError+ELNUtils.h"
+#import "NSFileManager+ELNUtils.h"
+#import "NSLocale+ELNUtils.h"
+#import "NSNumber+ELNUtils.h"
+#import "NSSortDescriptor+ELNUtils.h"
+#import "NSString+ELNUtils.h"
+#import "NSURLRequest+ELNUtils.h"
 
-
+#import "ELNLayoutUtils.h"
+#import "UICollectionView+ELNUtils.h"
+#import "UIColor+ELNUtils.h"
+#import "UIFont+ELNUtils.h"
+#import "UIImage+ELNUtils.h"
+#import "UILabel+ELNUtils.h"
+#import "UIMotionEffect+ELNUtils.h"
+#import "UINavigationController+ELNUtils.h"
+#import "UIResponder+ELNUtils.h"
+#import "UITableView+ELNUtils.h"
+#import "UITableViewCell+ELNUtils.h"
+#import "UIView+ELNUtils.h"
+#import "UIViewController+ELNUtils.h"
+#import "UIWindow+ELNUtils.h"
