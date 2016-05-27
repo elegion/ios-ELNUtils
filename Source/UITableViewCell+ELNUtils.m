cebd933cfcc78c0741c94bdbea1c74ffa26da36c
chebur <chebur.mail@gmail.com>
2016-05-27 12:14:41 +0300

    Update table view cell default insets

--- Source/UITableViewCell+ELNUtils.m
+++ Source/UITableViewCell+ELNUtils.m
@@ -8,11 +8,14 @@
 
 #import "UITableViewCell+ELNUtils.h"
 
+/// The side margins vary depending on the current size class, but can be either 16 or 20 points. You cannot change these margins.
+///
+/// @link https://developer.apple.com/library/ios/documentation/UIKit/Reference/UIView_Class/#//apple_ref/occ/instp/UIView/layoutMargins
 UIEdgeInsets ELNTableViewCellDefaultSeparatorInsets() {
     if ([UIScreen mainScreen].bounds.size.width >= 414) {
         return UIEdgeInsetsMake(0, 20, 0, 0);
     } else {
-        return UIEdgeInsetsMake(0, 15, 0, 0);
+        return UIEdgeInsetsMake(0, 16, 0, 0);
     }
 }
 
