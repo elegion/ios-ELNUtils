8d2e099f79fb02af43a0d9990de8362f6513fc6f
Dmitry Nesterenko <chebur@Dmitrys-MacBook.local>
2016-11-29 15:52:16 +0300

    Reimplement UIWindow extension in swift

--- Sources/UIWindow+Extension.swift
+++ Sources/UIWindow+Extension.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 
-extension UIWindow {
-    
-    enum RootViewControllerTransitionAnimation {
+public extension UIWindow {
+
+    public enum RootViewControllerTransitionAnimation {
         
         case none
         
@@ -18,16 +18,92 @@ extension UIWindow {
         
         case flipFromRight
         
+        case curlUp
+        
+        case curlDown
+
         case crossDissolve
 
+        case flipFromTop
+        
+        case flipFromBottom
+        
+        var animationOptions: UIViewAnimationOptions {
+            var options: UIViewAnimationOptions = [.allowAnimatedContent]
+            switch self {
+            case .none:
+                ()
+            case .flipFromLeft:
+                options.formUnion(.transitionFlipFromLeft)
+            case .flipFromRight:
+                options.formUnion(.transitionFlipFromRight)
+            case .curlUp:
+                options.formUnion(.transitionCurlUp)
+            case .curlDown:
+                options.formUnion(.transitionCurlDown)
+            case .crossDissolve:
+                options.formUnion(.transitionCrossDissolve)
+            case .flipFromTop:
+                options.formUnion(.transitionFlipFromTop)
+            case .flipFromBottom:
+                options.formUnion(.transitionFlipFromBottom)
+            }
+            return options
+        }
+        
+        var isAnimated: Bool {
+            return self != .none
+        }
+        
     }
     
     /**
      Replaces root view controller with specified animation.
      Often used by the auth <-> main screens transition.
      */
-    func eln_replaceRootViewController(with viewController: UIViewController, animation: RootViewControllerTransitionAnimation, completion: (Bool) -> Void) {
+    public func eln_replaceRootViewController(with viewController: UIViewController, animation: RootViewControllerTransitionAnimation, completion: @escaping (Bool) -> Void) {
+        guard let rootViewController = rootViewController, rootViewController.isViewLoaded, rootViewController.view.layer.presentation() != nil else {
+            self.rootViewController = viewController
+            return
+        }
+
+        let completion_: (Bool) -> Void = { finished in
+            // dismiss any previously presented modal view controllers
+            if rootViewController.presentedViewController != nil {
+                self.eln_dismissPresentedViewControllers(for: rootViewController, completion: {
+                    rootViewController.view.removeFromSuperview()
+                    completion(finished)
+                })
+            } else {
+                completion(finished)
+            }
+        }
+
+        if animation.isAnimated, let rootViewControllerView = rootViewController.view.snapshotView(afterScreenUpdates: false) {
+            self.rootViewController = rootViewController
+            self.rootViewController?.view.addSubview(rootViewControllerView)
+            UIView.transition(with: self, duration: 0.33, options: animation.animationOptions, animations: {
+                rootViewControllerView.removeFromSuperview()
+            }, completion: completion_)
+        } else {
+            self.rootViewController = viewController
+            completion_(true)
+        }
+    }
+   
+    private func eln_dismissPresentedViewControllers(for viewController: UIViewController, completion: (() -> ())? = nil) {
+        guard viewController.presentedViewController != nil else {
+            completion?()
+            return
+        }
         
+        viewController.dismiss(animated: false) { [weak self, weak viewController] in
+            guard let viewController = viewController else {
+                return
+            }
+
+            self?.eln_dismissPresentedViewControllers(for: viewController, completion: completion)
+        }
     }
     
 }
