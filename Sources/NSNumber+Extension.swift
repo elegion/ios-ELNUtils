99d9c212116a3a528f930fc31a06606320e0d7ab
Dmitry Nesterenko <chebur@Dmitrys-MacBook.local>
2016-11-29 17:36:08 +0300

    Implement NSNumber extension and remove NSSortDescriptor extensions

---
+++ Sources/NSNumber+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  NSNumber+Extension.swift
+//  ELNUtils
+//
+//  Created by Dmitry Nesterenko on 29/11/2016.
+//  Copyright © 2016 e-legion. All rights reserved.
+//
+
+import Foundation
+
+extension NSNumber {
+    
+    /// Returns a number of digits before floating point.
+    public var eln_numberOfIntegerDigits: UInt {
+        let value = abs(intValue)
+        if value == 0 {
+            return 1
+        } else {
+            return UInt(log10(CGFloat(value)) + 1)
+        }
+    }
+    
+}
