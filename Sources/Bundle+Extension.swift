7942567c849147ddd74cb6ef20006905a9b8e33a
Dmitry Nesterenko <chebur@Dmitrys-MacBook.local>
2016-11-29 17:30:24 +0300

    Implement locale and bundle extensions

---
+++ Sources/Bundle+Extension.swift
@@ -0,0 +1,52 @@
+//
+//  Bundle+Extension.swift
+//  ELNUtils
+//
+//  Created by Dmitry Nesterenko on 29/11/2016.
+//  Copyright © 2016 e-legion. All rights reserved.
+//
+
+import Foundation
+
+public extension Bundle {
+
+    private enum InfoDictionaryKey : String {
+        
+        case urlTypes = "CFBundleURLTypes"
+        
+        case urlSchemes = "CFBundleURLSchemes"
+        
+        case shortVersionString = "CFBundleShortVersionString"
+        
+        case displayName = "CFBundleDisplayName"
+        
+        case name = "CFBundleName"
+        
+    }
+   
+    /// Returns a union of CFBundleURLSchemes values declared in bundle's plist.
+    public var eln_urlSchemes: [Any]?  {
+        let keyPath = "\(InfoDictionaryKey.urlTypes).@unionOfArrays.\(InfoDictionaryKey.urlSchemes)"
+        let value = (infoDictionary as NSDictionary?)?.value(forKeyPath: keyPath) as? [Any]
+        return value
+    }
+
+    /// Returns a value for kCFBundleVersionKey key decalred in bundle's plist.
+    public var eln_version: String?  {
+        return infoDictionary?[kCFBundleVersionKey as String] as? String
+    }
+
+    /// Returns a value for CFBundleShortVersionString key declared in bundle's plist.
+    public var eln_shortVersion: String? {
+        return infoDictionary?[InfoDictionaryKey.shortVersionString.rawValue] as? String
+    }
+
+    public var eln_displayName: String? {
+        return infoDictionary?[InfoDictionaryKey.displayName.rawValue] as? String
+    }
+
+    public var eln_name: String? {
+        return infoDictionary?[InfoDictionaryKey.name.rawValue] as? String
+    }
+    
+}
---
+++ Sources/Locale+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  NSLocale+Extension.swift
+//  ELNUtils
+//
+//  Created by Dmitry Nesterenko on 29/11/2016.
+//  Copyright © 2016 e-legion. All rights reserved.
+//
+
+import Foundation
+
+public extension Locale {
+    
+    /// Return locale for main bundle's most preferred localization
+    /// Often preferred localization language and current locale differs. So in order to properly format
+    /// numbers and dates you should use this method to obtain user's current UI language.
+    public var eln_localeForPreferredLocalization: Locale {
+        let preferredLocalization = Bundle.main.preferredLocalizations.first!
+        return Locale(identifier: preferredLocalization)
+    }
+    
+}
